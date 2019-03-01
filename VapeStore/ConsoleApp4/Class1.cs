65c880b39ccc1887bdc5783c2a50e9aa4467e5f4
roqzera <48140171+roqzera@users.noreply.github.com>
2019-03-01 19:56:27 -0300

    Add files via upload

---
+++ VapeStore/ConsoleApp4/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoqueVapeStore
+{
+
+    public class CalculosCaixa
+    {
+        public decimal Valor;
+        public int Qtd;
+        public decimal Dinheiro;
+
+        public decimal Total()
+        {
+            return Qtd * Valor;
+        }
+        public decimal Troco()
+        {
+            return Dinheiro - Total();
+        }
+
+    }
+}
